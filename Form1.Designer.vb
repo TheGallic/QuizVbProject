04698feaf1481a6add79153b0345a127631f88a7
David <David@DESKTOP-3KMC9AQ>
2022-06-21 18:37:09 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CbxDifficult = New System.Windows.Forms.ComboBox()
+        Me.LblSCore = New System.Windows.Forms.Label()
+        Me.LblSteps = New System.Windows.Forms.Label()
+        Me.RtbQuestion = New System.Windows.Forms.RichTextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.BtnPlay = New System.Windows.Forms.Button()
+        Me.BtnValidate = New System.Windows.Forms.Button()
+        Me.RbA = New System.Windows.Forms.RadioButton()
+        Me.RbB = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RbD = New System.Windows.Forms.RadioButton()
+        Me.RbC = New System.Windows.Forms.RadioButton()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(162, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Choisissez la difficulté"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(214, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(49, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Score"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(348, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 21)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Etapes"
+        '
+        'CbxDifficult
+        '
+        Me.CbxDifficult.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CbxDifficult.FormattingEnabled = True
+        Me.CbxDifficult.Items.AddRange(New Object() {"Facile", "Normal", "Difficile", "Expert"})
+        Me.CbxDifficult.Location = New System.Drawing.Point(12, 33)
+        Me.CbxDifficult.Name = "CbxDifficult"
+        Me.CbxDifficult.Size = New System.Drawing.Size(162, 29)
+        Me.CbxDifficult.TabIndex = 4
+        '
+        'LblSCore
+        '
+        Me.LblSCore.AutoSize = True
+        Me.LblSCore.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LblSCore.Location = New System.Drawing.Point(214, 41)
+        Me.LblSCore.Name = "LblSCore"
+        Me.LblSCore.Size = New System.Drawing.Size(109, 21)
+        Me.LblSCore.TabIndex = 5
+        Me.LblSCore.Text = "Score: 0 sur 0"
+        '
+        'LblSteps
+        '
+        Me.LblSteps.AutoSize = True
+        Me.LblSteps.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.LblSteps.Location = New System.Drawing.Point(348, 41)
+        Me.LblSteps.Name = "LblSteps"
+        Me.LblSteps.Size = New System.Drawing.Size(119, 21)
+        Me.LblSteps.TabIndex = 6
+        Me.LblSteps.Text = "Etape: 0 sur 10"
+        '
+        'RtbQuestion
+        '
+        Me.RtbQuestion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RtbQuestion.ForeColor = System.Drawing.Color.DarkGreen
+        Me.RtbQuestion.Location = New System.Drawing.Point(12, 142)
+        Me.RtbQuestion.Name = "RtbQuestion"
+        Me.RtbQuestion.ReadOnly = True
+        Me.RtbQuestion.Size = New System.Drawing.Size(348, 197)
+        Me.RtbQuestion.TabIndex = 7
+        Me.RtbQuestion.Text = "En attente d'une nouvelle partie"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 118)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(80, 21)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Questions"
+        '
+        'BtnPlay
+        '
+        Me.BtnPlay.Location = New System.Drawing.Point(140, 371)
+        Me.BtnPlay.Name = "BtnPlay"
+        Me.BtnPlay.Size = New System.Drawing.Size(220, 32)
+        Me.BtnPlay.TabIndex = 9
+        Me.BtnPlay.Text = "Jouer"
+        Me.BtnPlay.UseVisualStyleBackColor = True
+        '
+        'BtnValidate
+        '
+        Me.BtnValidate.Enabled = False
+        Me.BtnValidate.Location = New System.Drawing.Point(414, 371)
+        Me.BtnValidate.Name = "BtnValidate"
+        Me.BtnValidate.Size = New System.Drawing.Size(220, 32)
+        Me.BtnValidate.TabIndex = 10
+        Me.BtnValidate.Text = "Valider la réponse"
+        Me.BtnValidate.UseVisualStyleBackColor = True
+        '
+        'RbA
+        '
+        Me.RbA.AutoSize = True
+        Me.RbA.Checked = True
+        Me.RbA.Location = New System.Drawing.Point(6, 28)
+        Me.RbA.Name = "RbA"
+        Me.RbA.Size = New System.Drawing.Size(102, 25)
+        Me.RbA.TabIndex = 11
+        Me.RbA.TabStop = True
+        Me.RbA.Text = "Réponse A"
+        Me.RbA.UseVisualStyleBackColor = True
+        '
+        'RbB
+        '
+        Me.RbB.AutoSize = True
+        Me.RbB.Location = New System.Drawing.Point(6, 59)
+        Me.RbB.Name = "RbB"
+        Me.RbB.Size = New System.Drawing.Size(101, 25)
+        Me.RbB.TabIndex = 12
+        Me.RbB.Text = "Réponse B"
+        Me.RbB.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RbD)
+        Me.GroupBox1.Controls.Add(Me.RbC)
+        Me.GroupBox1.Controls.Add(Me.RbA)
+        Me.GroupBox1.Controls.Add(Me.RbB)
+        Me.GroupBox1.Location = New System.Drawing.Point(366, 142)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(341, 188)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Réponses"
+        '
+        'RbD
+        '
+        Me.RbD.AutoSize = True
+        Me.RbD.Location = New System.Drawing.Point(6, 121)
+        Me.RbD.Name = "RbD"
+        Me.RbD.Size = New System.Drawing.Size(103, 25)
+        Me.RbD.TabIndex = 14
+        Me.RbD.Text = "Réponse D"
+        Me.RbD.UseVisualStyleBackColor = True
+        '
+        'RbC
+        '
+        Me.RbC.AutoSize = True
+        Me.RbC.Location = New System.Drawing.Point(6, 90)
+        Me.RbC.Name = "RbC"
+        Me.RbC.Size = New System.Drawing.Size(102, 25)
+        Me.RbC.TabIndex = 13
+        Me.RbC.Text = "Réponse C"
+        Me.RbC.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(705, 439)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.BtnValidate)
+        Me.Controls.Add(Me.BtnPlay)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.RtbQuestion)
+        Me.Controls.Add(Me.LblSteps)
+        Me.Controls.Add(Me.LblSCore)
+        Me.Controls.Add(Me.CbxDifficult)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Jeu Quiz"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CbxDifficult As ComboBox
+    Friend WithEvents LblSCore As Label
+    Friend WithEvents LblSteps As Label
+    Friend WithEvents RtbQuestion As RichTextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents BtnPlay As Button
+    Friend WithEvents BtnValidate As Button
+    Friend WithEvents RbA As RadioButton
+    Friend WithEvents RbB As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RbD As RadioButton
+    Friend WithEvents RbC As RadioButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,194 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+Public Class Form1
+
+    Private stepsNBR As Integer = 0
+    Private countSteps As Integer = 0
+    Private countAnswer As Integer = 0
+    Private gameDifficult As String = Nothing
+    Private gameResponse As String = Nothing
+    Private playerScore As Integer = 0
+    Private nbrOfQuestions As Integer = 0
+    Private memoryRead As New List(Of Integer)
+    Private AllAnswers As New List(Of String)
+    Private Sub BtnPlay_Click(sender As Object, e As EventArgs) Handles BtnPlay.Click
+        If CbxDifficult.SelectedItem = "" Then
+            MessageBox.Show("Vous devez sélectionner une difficulté")
+        Else
+            stepsNBR = 0
+            playerScore = 0
+            AllAnswers.Clear()
+            memoryRead.Clear()
+            CbxDifficult.Enabled = False
+            BtnValidate.Enabled = True
+            BtnPlay.Enabled = False
+
+            startGame()
+        End If
+    End Sub
+
+    Private Sub BtnValidate_Click(sender As Object, e As EventArgs) Handles BtnValidate.Click
+        Dim thisReponse As String = ""
+        'On regarde quel radio bouton est cocher puis on va chercher la reponse correspondante
+        If RbA.Checked = True Then
+            thisReponse = AllAnswers.ElementAt(0)
+        ElseIf RbB.Checked = True Then
+            thisReponse = AllAnswers.ElementAt(1)
+        ElseIf RbC.Checked = True Then
+            thisReponse = AllAnswers.ElementAt(2)
+        ElseIf RbD.Checked = True Then
+            thisReponse = AllAnswers.ElementAt(3)
+        End If
+        'On ajoute +1 au score si la condition est vrai
+        If getReponse(thisReponse) = True Then
+            playerScore += 1
+        End If
+
+        'On reinitialise certain elements
+        AllAnswers.Clear()
+        stepsNBR += 1
+        startGame()
+    End Sub
+
+    Public Sub startGame()
+        'On efface les anciennes valeurs
+        GameReset()
+
+        'On defini le niveau de  difficulté
+        gameDifficult = CbxDifficult.SelectedItem
+        'On lit le fichier contenant les informations du quizz
+        proceedFile()
+
+        'On met a jour le score
+        LblSCore.Text = "Score: " & playerScore & " sur " & getNbrOfQuestions()
+        LblSteps.Text = "Etapes: " & stepsNBR & " sur " & getNbrOfQuestions()
+
+        'Si la partie est finie on reinitialise
+        If stepsNBR = nbrOfQuestions Then
+            BtnPlay.Enabled = True
+            BtnValidate.Enabled = False
+            CbxDifficult.Enabled = True
+            GameReset()
+            RtbQuestion.Text = "En attente d'une nouvelle partie"
+            countSteps = 0
+            countAnswer = 0
+
+            'Si le joueur a le score maximum
+            If playerScore = stepsNBR Then
+                MessageBox.Show("Félicitation, vous avez fait un score parfait!!")
+            Else
+                MessageBox.Show("Vous pouvez vous améliorer!!")
+            End If
+        End If
+    End Sub
+
+    Public Sub proceedFile()
+        ' Correspond au nombre de lignes dans le fichier quizz
+        Dim NbrLine As Integer = 35 - 1
+        Dim myNumber As Integer = 0
+        'On genere un nombre aléatoire qui correspondra au numéro de la ligne à lire
+        Dim random As Random = New Random()
+        myNumber = random.Next(0, NbrLine)
+        'On verifie que le nombre n'a pas déjà été trouver, pour evité de relire la meme question
+        If memoryRead.Contains(myNumber) = True Then
+            While memoryRead.Contains(myNumber) = True
+                myNumber = random.Next(0, NbrLine)
+            End While
+        End If
+
+        'On ajoute le nombre aléatoire a la collections des ligne déjà lu
+        memoryRead.Add(myNumber)
+
+        'On lit le fichier a la ligne generer par la fonction random
+        Dim thisLine As String
+        Dim splitLine() As String = Nothing
+        Dim thisQuestion As String
+        Dim thisAnswers As String
+        Dim lines As Integer = 0
+        Dim monStringReader As New StringReader(My.Resources.Resource1.Quizz)
+        'On lit le fichier texte du quiz
+        While monStringReader.Peek <> -1
+            monStringReader.ReadLine()
+            If lines = myNumber Then
+                thisLine = monStringReader.ReadLine()
+                Exit While
+            End If
+
+            lines += 1
+        End While
+
+        'On decoupe la ligne de texte en question et réponses
+        splitLine = thisLine.Split("=")
+        thisQuestion = splitLine(0)
+        thisAnswers = splitLine(1)
+        RtbQuestion.Text = thisQuestion
+
+        'On redecoupe pour obtenir les réponses
+        Dim theRest() As String = Nothing
+        theRest = thisAnswers.Split(";")
+
+        Dim answerA As String = theRest(0)
+        AllAnswers.Add(answerA)
+        RbA.Text = answerA.Substring(2, answerA.Length - 2)
+
+        Dim answerB As String = theRest(1)
+        AllAnswers.Add(answerB)
+        RbB.Text = answerB.Substring(2, answerB.Length - 2)
+
+        If theRest.Length = 2 Then
+            RbC.Enabled = False
+            RbD.Enabled = False
+        ElseIf theRest.Length = 3 Then
+            Dim answerC As String = theRest(2)
+            AllAnswers.Add(answerC)
+            RbC.Text = answerC.Substring(2, answerC.Length - 2)
+            RbC.Enabled = True
+            RbD.Enabled = False
+        ElseIf theRest.Length = 4 Then
+            Dim answerC As String = theRest(2)
+            AllAnswers.Add(answerC)
+            RbC.Text = answerC.Substring(2, answerC.Length - 2)
+            RbC.Enabled = True
+
+            Dim answerD As String = theRest(3)
+            AllAnswers.Add(answerD)
+            RbD.Text = answerD.Substring(2, answerD.Length - 2)
+            RbD.Enabled = True
+        End If
+
+        monStringReader.Close()
+    End Sub
+
+
+    Public Sub GameReset()
+        'On efface les anciennes valeurs
+        RbA.Checked = True
+        RbA.Text = "Réponse A"
+        RbB.Text = "Réponse B"
+        RbC.Text = "Réponse C"
+        RbD.Text = "Réponse D"
+    End Sub
+
+    'Renvoie le nombres de questions suivant la difficulté
+    Public Function getNbrOfQuestions()
+        If gameDifficult = "Facile" Then
+            nbrOfQuestions = 10
+        ElseIf gameDifficult = "Normal" Then
+            nbrOfQuestions = 15
+        ElseIf gameDifficult = "Difficile" Then
+            nbrOfQuestions = 20
+        ElseIf gameDifficult = "Expert" Then
+            nbrOfQuestions = 30
+        End If
+        Return nbrOfQuestions
+    End Function
+    'On decoupe pour séparer la valeur de la reponse et la réponse
+    Public Function getReponse(thisReponse As String) As Boolean
+        gameResponse = thisReponse.Substring(0, 1)
+        If gameResponse.Contains("1") = True Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
